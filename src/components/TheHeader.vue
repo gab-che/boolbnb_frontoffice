f57337ebfc39ed9f8c357cdde8a7d21cd900fbb0
Ciaowilly3 <muscomassi@gmail.com>
2023-03-07 17:23:18 +0100

    created an home vue

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,24 @@
+<script>
+export default {};
+</script>
+<template>
+  <nav class="navbar navbar-light shadow">
+    <div class="container">
+      <div class="logo-container"><img src="../assets/vue.svg" alt="" /></div>
+      <div class="link-container">
+        <ul class="d-flex justify-content-beetween">
+          <li><a href=""></a>link</li>
+          <li><a href=""></a>link</li>
+          <li><a href=""></a>link</li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+<style lang="scss" scoped>
+ul {
+  list-style: none;
+  margin-block-start: 0.5em;
+  padding-inline-start: 0px;
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,15 @@
 <script>
-export default{
-    name: "Home"
-}
+import Carousel from "../components/Carousel.vue";
+import TheHeader from "../components/TheHeader.vue";
+export default {
+  components: { Carousel, TheHeader },
+  name: "Home",
+};
 </script>
 
 <template>
-
+  <TheHeader></TheHeader>
+  <Carousel></Carousel>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
