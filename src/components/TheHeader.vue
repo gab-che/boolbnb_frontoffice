401d5d4aa2600a6e871791d9a9dde0276b05c946
Ciaowilly3 <muscomassi@gmail.com>
2023-03-10 13:05:08 +0100

    modified home added jumbo and search

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -4,27 +4,42 @@ export default {};
 <template>
   <header>
     <nav class="navbar navbar-light shadow">
-      <div class="container">
-        <div class="logo-container">
-          <img src="../../public/logo_nobg.png" alt="" />
-        </div>
-        <div class="link-container">
-          <ul class="d-flex justify-content-beetween">
-            <li><router-link :to="{ name: 'search' }">Ricerca</router-link></li>
-            <li><router-link :to="{ name: 'search' }">Ricerca</router-link></li>
-            <li><router-link :to="{ name: 'search' }">Ricerca</router-link></li>
-          </ul>
-        </div>
+      <div class="logo-container">
+        <img src="../../logo_nobg.png" alt="" />
+      </div>
+      <div class="search-container">
+        <input type="text" name="" class="search" />
+      </div>
+      <div class="link-container">
+        <ul class="d-flex justify-content-beetween">
+          <li><router-link :to="{ name: 'search' }">Ricerca</router-link></li>
+          <li><router-link :to="{ name: 'search' }">Ricerca</router-link></li>
+          <li><router-link :to="{ name: 'search' }">Ricerca</router-link></li>
+        </ul>
       </div>
     </nav>
   </header>
 </template>
 <style lang="scss" scoped>
+.search {
+  font-size: 16px;
+  font-size: max(16px, 1em);
+  font-family: inherit;
+  padding: 0.25em 0.5em;
+  background-color: #fff;
+  border: 2px solid rgb(208, 190, 25);
+  border-radius: 4px;
+}
+.search:focus-visible {
+  outline: 2px solid rgb(47, 34, 11);
+  outline-offset: 2px;
+}
 img {
   width: 120px;
 }
 nav {
   background-color: rgb(0, 0, 0);
+  padding: 0px 64px;
 }
 ul {
   list-style: none;
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,17 @@
 <script>
 import Carousel from "../components/Carousel.vue";
 import TheHeader from "../components/TheHeader.vue";
+import TheJumbotron from "../components/TheJumbotron.vue";
 export default {
-  components: { Carousel, TheHeader },
+  components: { Carousel, TheHeader, TheJumbotron },
   name: "Home",
 };
 </script>
 
 <template>
   <TheHeader></TheHeader>
-  <Carousel></Carousel>
+  <TheJumbotron></TheJumbotron>
+  <!-- <Carousel></Carousel> -->
 </template>
 
 <style lang="scss" scoped></style>
