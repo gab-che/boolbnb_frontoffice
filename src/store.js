01b5fb6f4813fc316740db453afd36d8f08fd307
Ciaowilly3 <muscomassi@gmail.com>
2023-03-13 16:25:41 +0100

    addded function to get services

--- src/store.js
+++ src/store.js
@@ -1,52 +1,55 @@
-import { reactive } from 'vue';
-import axios from 'axios';
+import { reactive } from "vue";
+import axios from "axios";
 
 export const store = reactive({
-    backendApartments: 'http://localhost:8000/api/apartments/',
-    backendMessages: 'http://localhost:8000/api/messages/',
-    backendVisuals: 'http://localhost:8000/api/visuals/',
+  backendApartments: "http://localhost:8000/api/apartments/",
+  backendMessages: "http://localhost:8000/api/messages/",
+  backendVisuals: "http://localhost:8000/api/visuals/",
+  backendservices: "http://localhost:8000/api/services/",
 
-    allApartments: [],
-    nearestApartments: [],
-    sponsoredApartments: [],
-    singleApartment: {},
+  allApartments: [],
+  nearestApartments: [],
+  sponsoredApartments: [],
+  singleApartment: {},
+  allServices: [],
 
-    currentPage: 1,
-    totalPages: 1,
+  currentPage: 1,
+  totalPages: 1,
 });
 
 /**
  * Restituisce appartamenti paginati
  */
 export function fetchAllApartments(query) {
-    axios.get(store.backendApartments, {
-        params: {
-            ...query,
-        }
+  axios
+    .get(store.backendApartments, {
+      params: {
+        ...query,
+      },
     })
-        .then((resp) => {
-            store.allApartments = resp.data.data;
-            store.currentPage = resp.data.current_page;
-            store.totalPages = resp.data.last_page;
-        })
-};
+    .then((resp) => {
+      store.allApartments = resp.data.data;
+      store.currentPage = resp.data.current_page;
+      store.totalPages = resp.data.last_page;
+    });
+}
 
 export function fetchSingleApartment(id) {
-    axios.get(store.backendApartments + id)
-        .then((resp) => {
-            store.singleApartment = resp.data;
-        })
+  axios.get(store.backendApartments + id).then((resp) => {
+    store.singleApartment = resp.data;
+  });
 }
 
 export function fetchSponsoredApartments() {
-    axios.get(store.backendApartments, {
-        params: {
-            sponsored: true,
-        }
+  axios
+    .get(store.backendApartments, {
+      params: {
+        sponsored: true,
+      },
     })
-        .then((resp) => {
-            store.sponsoredApartments = resp.data;
-        })
+    .then((resp) => {
+      store.sponsoredApartments = resp.data;
+    });
 }
 
 /**
@@ -54,14 +57,21 @@ export function fetchSponsoredApartments() {
  * dopo ricerca semplice (es. solo città)
  */
 export function fetchNearestApartments(cityInput) {
-    axios.get(store.backendApartments, {
-        params: {
-            //input da passare con v-model
-            city: cityInput,
-        }
+  axios
+    .get(store.backendApartments, {
+      params: {
+        //input da passare con v-model
+        city: cityInput,
+      },
     })
-        .then((resp) => {
-            console.log(resp.data);
-            store.nearestApartments = resp.data;
-        })
+    .then((resp) => {
+      console.log(resp.data);
+      store.nearestApartments = resp.data;
+    });
+}
+
+export function fetchServices() {
+  axios.get(store.backendservices, {}).then((resp) => {
+    store.allServices = resp.data;
+  });
 }
