5181b16fb82f0e73ece8372d94c54fb8587d2e95
Ciaowilly3 <muscomassi@gmail.com>
2023-03-13 23:30:02 +0100

    some changes

---
+++ src/components/ModalForSearch.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- <h4>abda</h4> -->
+</template>
+<style
+  lang="scss
+  "
+  scoped
+></style>
+
+<script>
+export default {};
+</script>
--- src/pages/AdvancedSearch.vue
+++ src/pages/AdvancedSearch.vue
@@ -1,13 +1,41 @@
 <script>
-export default{
-    name: "AdvancedSearch"
-}
+import { store, fetchServices } from "../store";
+import axios from "axios";
+import { onMounted } from "vue";
+// import ModalForSearch from "../components/ModalForSearch.Vue";
+import TheHeader from "../components/TheHeader.vue";
+export default {
+  components: { TheHeader },
+  name: "AdvancedSearch",
+  data() {
+    return {
+      store,
+      services: fetchServices(),
+    };
+  },
+  methods: {},
+  mounted() {
+    fetchServices();
+  },
+};
 </script>
 
 <template>
-
+  <TheHeader></TheHeader>
+  <div class="services-container">
+    <ul>
+      <li v-for="service in services">
+        <a href="#" class="text-white text-decoration-none">
+          {{ service.name }}
+          <div><i :class="service.icon"></i></div>
+        </a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-
+.service-container {
+  background-color: rgb(32, 30, 30);
+}
 </style>
