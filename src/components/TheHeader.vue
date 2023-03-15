d266559ae56d7817218eb70674ae379edd1a97bb
Ciaowilly3 <muscomassi@gmail.com>
2023-03-15 17:33:58 +0100

    leaved input from about

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,16 +1,10 @@
 <script>
-import { fetchNearestApartments } from "../store";
+import SimpleSearch from "./SimpleSearch.vue";
 export default {
-  data() {
-    return {
-      input: "",
-    };
-  },
+  components: { SimpleSearch },
+  data() {},
   methods: {
-    sendInput() {
-      fetchNearestApartments(this.input);
-      this.$router.push("/ricerca-avanzata");
-    },
+    sendInput() {},
   },
 };
 </script>
@@ -49,15 +43,7 @@ export default {
             </li>
           </ul>
         </div>
-        <div class="search-container">
-          <input
-            type="text"
-            name=""
-            class="search"
-            @keyup.enter="sendInput"
-            v-model="input"
-          />
-        </div>
+        <SimpleSearch></SimpleSearch>
       </div>
     </nav>
   </div>
@@ -65,19 +51,6 @@ export default {
 <style lang="scss" scoped>
 .bg-my-dark {
   background-color: rgb(10, 7, 2);
-}
-.search {
-  font-size: 16px;
-  font-size: max(16px, 1em);
-  font-family: inherit;
-  padding: 0.25em 0.5em;
-  background-color: #fff;
-  border: 2px solid rgb(208, 190, 25);
-  border-radius: 4px;
-}
-.search:focus-visible {
-  outline: 2px solid rgb(47, 34, 11);
-  outline-offset: 2px;
 }
 nav {
   position: sticky;
