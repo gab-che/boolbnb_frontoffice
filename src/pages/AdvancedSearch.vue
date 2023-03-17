3c39f2aea1d2412c903d29d9a0c878c7f67a33b0
LucaRadio <luca.1407@hotmail.it>
2023-03-17 10:00:12 +0100

    Add image

--- src/pages/AdvancedSearch.vue
+++ src/pages/AdvancedSearch.vue
@@ -40,27 +40,29 @@ export default {
 </script>
 
 <template>
-  <div
-    v-if="store.allServices && store.nearestApartments"
-    class="all-container"
-  >
+  <div v-if="store.allServices && store.nearestApartments" class="all-container">
     <TheHeader></TheHeader>
     <!-- <div class="services-container p-2">
-      <div class="row g-2">
-        <div
-          class="col-lg-3 col-md-4 col-sm-8"
-          v-for="service in store.allServices"
-        >
-          <a href="#" class="text-white text-decoration-none">
-            {{ service.name }}
-            <div class="ms-auto"><i :class="service.icon"></i></div>
-          </a>
-        </div>
-      </div>
-    </div> -->
+                <div class="row g-2">
+                  <div
+                    class="col-lg-3 col-md-4 col-sm-8"
+                    v-for="service in store.allServices"
+                  >
+                    <a href="#" class="text-white text-decoration-none">
+                      {{ service.name }}
+                      <div class="ms-auto"><i :class="service.icon"></i></div>
+                    </a>
+                  </div>
+                </div>
+              </div> -->
 
     <ul v-for="apartment in store.nearestApartments">
       <li>{{ apartment.title }}</li>
+      <li>
+        <div class="img-container">
+          <img :src="'http://localhost:8000/storage/' + apartment.img_cover" alt="" class="img-fluid">
+        </div>
+      </li>
     </ul>
     <div v-if="store.nearestApartments.length === 0">
       <h3 class="banner banner-warning">Non hai risultati</h3>
@@ -69,15 +71,23 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+.img-container {
+  width: 300px;
+  height: 300px;
+}
+
 .all-container {
   width: 100vw;
 }
+
 .services-container {
   background-color: rgb(32, 30, 30);
   font-size: 24px;
+
   ul {
     list-style-type: none;
     display: flex;
+
     li {
       flex-grow: 1;
     }
