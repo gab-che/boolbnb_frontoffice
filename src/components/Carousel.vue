cdd649adcc9537dcb0467479f9f2bc690e0cbca2
Ciaowilly3 <muscomassi@gmail.com>
2023-03-18 18:05:35 +0100

    other fixes

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -7,24 +7,15 @@
     >
       <div class="carousel-indicators">
         <button
+          v-for="(apartment, i) in store.sponsoredApartments"
+          :key="i"
           type="button"
           data-bs-target="#carouselExampleCaptions"
-          data-bs-slide-to="0"
-          class="active"
-          aria-current="true"
-          aria-label="Slide 1"
-        ></button>
-        <button
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide-to="1"
-          aria-label="Slide 2"
-        ></button>
-        <button
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide-to="2"
-          aria-label="Slide 3"
+          :data-bs-slide-to="i"
+          :class="i === currentIndex ? 'active' : ''"
+          :aria-current="i === currentIndex ? 'true' : 'false'"
+          :aria-label="`'slide ' ${i + 1} `"
+          @click="currentIndex = i"
         ></button>
       </div>
       <div class="carousel-inner">
@@ -79,7 +70,10 @@ import { store, fetchSponsoredApartments } from "../store";
 
 export default {
   data() {
-    return { store };
+    return {
+      store,
+      currentIndex: 0,
+    };
   },
 
   mounted() {
@@ -108,6 +102,19 @@ img {
     rgba(255, 255, 255, 0.43469887955182074) 100%
   );
   transition: all 0.5s ease;
+  .carousel-caption {
+    width: fit-content;
+    margin: auto;
+    padding: 2px 8px;
+    border: 1px solid rgb(53, 34, 12);
+    border-radius: 12px;
+    backdrop-filter: blur(2px);
+    background-color: rgb(53, 34, 12, 0.6);
+    h5 {
+      margin: 0;
+      color: rgb(244, 194, 30);
+    }
+  }
 }
 @media screen and (min-width: 1240px) {
   .carousel {
